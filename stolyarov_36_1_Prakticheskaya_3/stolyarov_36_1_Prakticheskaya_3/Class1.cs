10dc048b6a709a53c22062717864a051d036b326
stolyarov-nikita <130557948+stolyarov-nikita@users.noreply.github.com>
2023-06-25 20:37:03 +0300

    Add files via upload

---
+++ stolyarov_36_1_Prakticheskaya_3/stolyarov_36_1_Prakticheskaya_3/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stolyarov_36_1_Prakticheskaya_3
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth;
+
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; } 
+            set { roomWidth = value; }
+        }
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        public double PersonArea(int numbOfPerson)
+        {
+            return RoomArea() / numbOfPerson;
+        }
+    }
+}
